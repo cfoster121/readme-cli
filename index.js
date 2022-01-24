63910a93cdc9154c72ec1bc5c793b07357e4bb84
Courtney Foster <cfoster649@gmail.com>
2022-01-23 21:41:07 -0800

    Created prompt sections

--- index.js
+++ index.js
@@ -2,4 +2,52 @@ var fs = require('fs');
 
 var inquirer = require('inquirer');
 
-console.log("test")
+
+inquirer
+.prompt([
+    {
+        message:
+        type:
+        name: 'title'
+    },
+    {
+        message:
+        type:
+        name: 'descripton'
+    },
+    {
+        message:
+        type:
+        name: 'table of contents'
+    },
+    {
+        message:
+        type:
+        name: 'installation'
+    },
+    {
+        message:
+        type:
+        name: 'usage'
+    },
+    {
+        message:
+        type:
+        name: 'license' 
+    },
+    {
+        message:
+        type:
+        name: 'contributing'
+    },
+    {
+        message:
+        type:
+        name: 'tests'
+    },
+    {
+        message:
+        type:
+        name: 'questions'
+    },
+])
