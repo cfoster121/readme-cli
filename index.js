29a030af899ce5371e355e859c15224546829364
Courtney Foster <cfoster649@gmail.com>
2022-01-24 18:34:45 -0800

    Added all license badges

--- index.js
+++ index.js
@@ -30,57 +30,38 @@ inquirer
             type: 'list',
             name: 'license',
             choices: [
-                // "Academic Free License v3.0",
-                {
-                    name: "Apache License 2.0",
-                    badge: "assets/images/apache2.png"
-                },
-                // "Artistic License 2.0",
-                {
-                    name: "Boost Software License 1.0",
-                    badge: "assets/images/boost1.png",
-                },
-                {
-                    name: "BSD 2-Clause Simplified License",
-                    badge: "assets/images/bsd2.png",
-                },
-                {
-                    name: "BSD 3-Clause Revised License",
-                    badge: "assets/images/bsd3.png",
-                },
-                
-                // "Creative Commons License Family",
-                {
-                    name: "Creative Commons Zero V1.0 Universal",
-                    badge: "assets/images/cc01.png",
-                }
-                // "Creative Commons Attribution 4.0",
-                // "Creative Commons Attribution Share Alike 4.0",
-                // "Creative Commons Attribution NonCommercial 4.0 International",
-                // "Creative Commons Attribution NoDerivitaves 4.0 International",
-                // "Creative Commons Attribution NonCommmercial-ShareAlike 4.0 International",
-                // "Creative Commons Attribution NonCommercial-NoDerivitaves 4.0 International",
-                // "Eclipse Public Licnense 1.0",
-                // "GNU GPL V3",
-                // "GNU GPL V2",
-                // "GNP AGPL V3",
-                // "GNU LGPL V3",
-                // "GNU FL V1.3",
-                // "The Hippocratic License 2.1",
-                // "The Hippocratic License 3.0",
-                // "IBM Public License Version 1.0",
-                // "ISC License",
-                // "The MIT License",
-                // "Mozilla Public License 2.0",
-                // "Attribution License (BY)",
-                // "Open Database License (ODbL)",
-                // "Public Domain Dedication and License (PDDL)",
-                // "The Perl License",
-                // "The Artistic License 2.0",
-                // "SIL Open Font License 1.1",
-                // "The Unlicense",
-                // "The Do What The Fuck You Want To Public License (WTFPL)",
-                // "Zlib"
+                "Apache License 2.0",
+                "Boost Software License 1.0",
+                "BSD 2-Clause Simplified License",
+                "BSD 3-Clause Revised License",
+                "Creative Commons Zero V1.0 Universal",
+                "Creative Commons Attribution 4.0",
+                "Creative Commons Attribution Share Alike 4.0",
+                "Creative Commons Attribution NonCommercial 4.0 International",
+                "Creative Commons Attribution NoDerivitaves 4.0 International",
+                "Creative Commons Attribution NonCommmercial-ShareAlike 4.0 International",
+                "Creative Commons Attribution NonCommercial-NoDerivitaves 4.0 International",
+                "Eclipse Public License 1.0",
+                "GNU GPL V3",
+                "GNU GPL V2",
+                "GNP AGPL V3",
+                "GNU LGPL V3",
+                "GNU FL V1.3",
+                "The Hippocratic License 2.1",
+                "The Hippocratic License 3.0",
+                "IBM Public License Version 1.0",
+                "ISC License",
+                "The MIT License",
+                "Mozilla Public License 2.0",
+                "Attribution License (BY)",
+                "Open Database License (ODbL)",
+                "Public Domain Dedication and License (PDDL)",
+                "The Perl License",
+                "The Artistic License 2.0",
+                "SIL Open Font License 1.1",
+                "The Unlicense",
+                "The Do What The Fuck You Want To Public License (WTFPL)",
+                "Zlib"
             ]
         },
         {
@@ -114,8 +95,8 @@ inquirer
         var badge = `${r.license}`.split(" ").join("-")
 
         const readMeTemplate =
-    
-    `
+
+            `
 
 # Title  
 ${r.title}   
